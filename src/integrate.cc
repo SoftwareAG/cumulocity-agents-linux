e12ecc7562acab89d2090a642f209a7a8c988d54
Tiensbakung <Tiensbakung@gmail.com>
2015-12-08 11:38:40 +0100

    integrate: use registerSrTemplate

--- src/integrate.cc
+++ src/integrate.cc
@@ -1,4 +1,5 @@
 #include <srnethttp.h>
+#include <srutils.h>
 #include "integrate.h"
 using namespace std;
 
@@ -7,26 +8,12 @@ int Integrate::integrate(const SrAgent &agent, const string &srv,
                          const string &srt)
 {
         SrNetHttp http(agent.server()+"/s", srv, agent.auth());
-        if (http.post("") < 0)
-                return -1;
-
-        SmartRest sr(http.response());
-        SrRecord r = sr.next();
-        if (r.size() && r[0].second == "40") {
+        if (registerSrTemplate(http, xid, srt) == 0) {
                 http.clear();
-                if (http.post(srt) <= 0)
+                if (http.post("300," + agent.deviceID()) <= 0)
                         return -1;
-                sr.reset(http.response());
-                r = sr.next();
-        }
-        if (r.size() == 2 && r[0].second == "20") {
-                xid = r[1].second;
-                string s = "300," + agent.deviceID();
-                http.clear();
-                if (http.post(s) <= 0)
-                        return -1;
-                sr.reset(http.response());
-                r = sr.next();
+                SmartRest sr(http.response());
+                SrRecord r = sr.next();
                 if (r.size() && r[0].second == "50") {
                         http.clear();
                         if (http.post("301,20") <= 0)
@@ -35,7 +22,7 @@ int Integrate::integrate(const SrAgent &agent, const string &srv,
                         r = sr.next();
                         if (r.size() == 3 && r[0].second == "801") {
                                 id = r[2].second;
-                                s = "302," + id + "," + agent.deviceID();
+                                string s = "302," + id + "," + agent.deviceID();
                                 if (http.post(s) <= 0)
                                         return -1;
                                 return 0;
