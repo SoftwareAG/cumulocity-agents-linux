93f9a8b2e98d306f565aa443e222028ad5e43b04
Tiensbakung <Tiensbakung@gmail.com>
2016-03-02 17:18:39 +0100

    lua/system: add support for system.mem.interval and system.cpu.interval in c8ydemo.conf

--- lua/system.lua
+++ lua/system.lua
@@ -1,10 +1,13 @@
 require('sysutils')
 local memTimer
 local cpuTimer
+local t0, t1
 function init()
+   t0 = os.time()
+   t1 = os.time()
    c8y:addMsgHandler(803, 'restartSystem')
-   memTimer = c8y:addTimer(300*1000, 'sendMemUsage')
-   cpuTimer = c8y:addTimer(300*1000, 'sendSystemLoad')
+   memTimer = c8y:addTimer(10*1000, 'sendMemUsage')
+   cpuTimer = c8y:addTimer(10*1000, 'sendSystemLoad')
    memTimer:start()
    cpuTimer:start()
    local model = command("dmidecode -s system-product-name")
@@ -23,13 +26,21 @@ end
 
 
 function sendMemUsage()
-   local total, use = getMemUsage()
-   local perc = string.format("%.1f", use*100/total)
-   c8y:send("305," .. c8y.ID .. ',' .. use .. ',' .. perc)
+   local t = os.time()
+   if t0 + cdb:get('system.mem.interval') <= t then
+      t0 = t
+      local total, use = getMemUsage()
+      local perc = string.format("%.1f", use*100/total)
+      c8y:send("305," .. c8y.ID .. ',' .. use .. ',' .. perc)
+   end
 end
 
 
 function sendSystemLoad()
-   local value = string.format("%.2f", getSystemLoad())
-   c8y:send("306," .. c8y.ID .. ',' .. value)
+   local t = os.time()
+   if t1 + cdb:get('system.cpu.interval') <= t then
+      t1 = t
+      local value = getSystemLoad()*100
+      c8y:send("306," .. c8y.ID .. ',' .. value)
+   end
 end
