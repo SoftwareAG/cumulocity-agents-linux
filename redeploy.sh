7594461d37421fbafecbff1d7c4b4a3a9c453878
Ivan Korolev <ivan.korolev@cumulocity.com>
2018-02-19 15:37:02 +0000

    [CP-731]redeploy.sh: fixes

--- redeploy.sh
+++ redeploy.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-sudo systemctl kill cumulocity-agent
+sudo systemctl stop cumulocity-agent
 sleep 1
 sudo make uninstall
-sleep 1
 sudo make install
-sleep 1
 sudo systemctl enable cumulocity-agent
-sleep 1
 sudo systemctl start cumulocity-agent
-
