d311f6c6f738ed44219d22ae3a8cd60b7f8214e4
Ivan Korolev <ivan.korolev@cumulocity.com>
2019-02-03 19:35:44 +0000

    [MTM-23756]cumulocity-agent: version upgraded to 2.1.4

--- lua/software.lua
+++ lua/software.lua
@@ -1,6 +1,6 @@
 local tbl = {}
 local agentName = "cumulocity-agent"
-local agentVersion = '2.1.3'
+local agentVersion = '2.1.4'
 local agentUrl = ''
 
 function init()
--- pkg/rpm/build_rpm.sh
+++ pkg/rpm/build_rpm.sh
@@ -6,7 +6,7 @@ TARGET_BASE=/usr/share/cumulocity-agent
 DATAPATH=/var/lib/cumulocity-agent
 PREFIX=/usr
 RPM_BASE=/tmp/rpmbuild
-AGENT_VERSION='2.1.3'
+AGENT_VERSION='2.1.4'
 RPM_RELEASE='1'
 
 
