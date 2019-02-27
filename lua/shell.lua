04df69b3d7d55e5dc89119ee55c680a20afbabe0
Tiensbakung <Tiensbakung@gmail.com>
2019-02-27 12:33:42 +0800

    [MTM-24961]shell: surround commands with double quotes for avoiding SmartREST parsing issues.

--- lua/shell.lua
+++ lua/shell.lua
@@ -1,17 +1,18 @@
 function init()
-   c8y:addMsgHandler(873, 'shell')
-   c8y:addMsgHandler(872, 'shell')
+   c8y:addMsgHandler(807, 'shell')
+   c8y:addMsgHandler(875, 'shell')
    return 0
 end
 
 
 function shell(r)
-   local opid, deviceId, line = r:value(2), r:value(3), r:value(4)
+   local opid, line, deviceId = r:value(2), r:value(3), r:value(4)
    if deviceId ~= c8y.ID then return end
    c8y:send('303,' .. opid .. ',EXECUTING')
    local cmd, key, value = string.match(line, '(%S+)%s+(%S+)%s*(%S*)')
+   line = '"' .. line .. '"'
    if cmd == 'get' then
-      local res = cdb:get(key)
+      local res = '"' .. cdb:get(key) .. '"'
       c8y:send(table.concat({'310', opid, 'SUCCESSFUL', line, res}, ','), 1)
    elseif cmd == 'set' then
       cdb:set(key, value or '')
